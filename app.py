9c1941840f03f2e589dacf0ad5f6ea03f69f4395
EmmanuelCruz24 <a357615@uach.mx>
2024-10-02 21:06:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+# Tarea: Dockerizando una aplicacion
+# Alumnno: Emmanuel Cruz
+# Matricula: 357615
+# Materia: Programacion para plataformas emergentes
+# Lenguaje de programacion: Python
+
+# app.py
+
+print("¡Hola! Estoy comprobando la comunicacion de mi aplicacion dockerizada.")
